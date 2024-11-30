baba25c3d674b68405f0287d0995fd0eb7288228
rklaumbach <rklaumbach@gmail.com>
2024-11-30 13:09:34 -0300

    spinner aesthetic improvement

--- nextjs/src/components/Spinner.tsx
+++ nextjs/src/components/Spinner.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Spinner: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-700"></div>
     </div>
   );
 };
